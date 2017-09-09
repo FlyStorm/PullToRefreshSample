389e0bb2cafc2b080852eac4ed50503d3ba10df3
FlyStorm <ggloading@gmail.com>
2017-09-09 18:33:55 +0800

    导入自己封装的下拉刷新的第三方库，并添加依赖

--- app/src/main/java/com/pulltorefreshsample/activity/MainActivity.java
+++ app/src/main/java/com/pulltorefreshsample/activity/MainActivity.java
@@ -1,13 +1,29 @@
 package com.pulltorefreshsample.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+    //声明控件
+    private Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //实例化控件
+        btn1= (Button) findViewById(R.id.btn1);
+
+        //设置button点击监听
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,RefreshListViewActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/pulltorefreshsample/activity/RefreshListViewActivity.java
@@ -0,0 +1,13 @@
+package com.pulltorefreshsample.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RefreshListViewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_refresh_list_view);
+    }
+}
